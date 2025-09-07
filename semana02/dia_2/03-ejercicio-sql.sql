43d3f92a720f0ebbded72a276b7bdb480c01bba5
Eduardo <ederiveroman@gmail.com>
2025-09-07 10:12:20 -0500

    faker

--- semana02/dia_2/03-ejercicio-sql.sql
+++ semana02/dia_2/03-ejercicio-sql.sql
@@ -1,4 +1,6 @@
 -- Crear una base de datos llamada colegio
+CREATE DATABASE colegio;
+\c colegio
 
 -- En la base de datos crear una tabla llamada profesores que va a tener la sgte info
 -- id serial llave primaria
@@ -8,4 +10,15 @@
 -- correo_institucional texto unico
 -- activo boolean 
 -- fecha_contratacion timestamp
+CREATE TABLE colegio (
+    id SERIAL PRIMARY KEY,
+    nombre TEXT NOT NULL,
+    apellidos TEXT NOT NULL,
+    correo TEXT UNIQUE,
+    correo_institucional TEXT UNIQUE,
+    activo BOOLEAN,
+    fecha_contratacion TIMESTAMP
+);
 
+-- Para cambiar el nombre de la tabla de colegio a profesores
+ALTER TABLE colegio RENAME TO profesores;
---
+++ semana02/dia_2/04_generador_data.py
@@ -0,0 +1,7 @@
+from faker import Faker
+
+# Creamos una instancia de nuestra clase
+# instancia: copia de toda la configuracion de la clase que va a ser almacenada en la variable
+creador = Faker()
+
+print(creador.name())

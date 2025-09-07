2987c6ac91da36ff563eb0d0087b0c09d0319e07
Eduardo <ederiveroman@gmail.com>
2025-09-07 11:51:31 -0500

    uso avanzado de select

--- semana02/dia_2/04_generador_data.py
+++ semana02/dia_2/04_generador_data.py
@@ -4,12 +4,34 @@ from datetime import date
 # instancia: copia de toda la configuracion de la clase que va a ser almacenada en la variable
 creador = Faker()
 
-print(creador.name())
+texto = 'INSERT INTO profesores (nombre, apellidos, correo, correo_institucional, activo, fecha_contratacion) VALUES '
 
-nombre = creador.first_name()
-apellidos = creador.last_name()
-correo = creador.email(True)
-correo_institucional = creador.email(True,'codigo.edu.pe')
-activo = creador.boolean()
-fecha_contratacion = creador.date_between(start_date=date(2022,1,15),end_date=date(2025,8,1))
+for numero in range(100):
+    nombre = creador.first_name()
+    apellidos = creador.last_name()
+    correo = creador.email(True)
+    correo_institucional = creador.email(True,'codigo.edu.pe')
+    activo = creador.boolean()
+    fecha_contratacion = creador.date_between(start_date=date(2022,1,15),end_date=date(2025,8,1))
 
+    data = "('{}','{}','{}','{}',{},'{}'),".format(nombre,
+                                                  apellidos,
+                                                  correo,
+                                                  correo_institucional,
+                                                  activo,
+                                                  fecha_contratacion)
+
+    # Al ser string no se hara sumatoria sino se realizara una concatenacion
+    texto += data
+    # En el ultimo registro en vez de colocar una coma se coloque un `;`
+
+resultado = texto.rsplit(',',1)[0] + ';'
+
+print(resultado)
+
+# agregando al comando `> NOMBRE_ARCHIVO.EXT` en vez de mostrarlo en la terminal las impresiones de pantalla lo guardara en ese archivo definido en la terminal
+
+
+# Para leer este archivo en la terminal
+# MAC | Powershell | GitBash : cat
+# CMD (windows): type
---
+++ semana02/dia_2/05_funciones_agregacion.sql
@@ -0,0 +1,47 @@
+-- Retornara el numero total de registros
+SELECT count(*) FROM profesores;
+
+-- Si queremos agregar una columna "normal" tenemos que obligatoriamente usar el group by para que realice primero la agrupacion de estos elementos y luego proceda con el conteo de los mismos
+SELECT count(*), activo FROM profesores GROUP BY activo;
+
+SELECT * FROM profesores ORDER BY nombre ASC; -- ASC: A - Z | DESC: Z - A
+
+-- Si tenemos dos ordenamientos el segundo entrara a funcionar si hay dos coincidencias con el primero 
+SELECT * FROM profesores ORDER BY apellidos ASC, nombre ASC; -- ASC: A - Z | DESC: Z - A
+
+-- Paginacion
+-- OFFSET: indica cuantos registros me tengo que saltar 
+SELECT * FROM profesores OFFSET 5;
+-- LIMIT : indica cuantos registros quiero devolver
+SELECT * FROM profesores LIMIT 5;
+
+-- Agarrando los 5 primeros registros
+SELECT * FROM profesores LIMIT 5 OFFSET 0;
+
+-- Seleccionando los 5 segundos registros
+SELECT * FROM profesores LIMIT 5 OFFSET 5;
+SELECT * FROM profesores LIMIT 5 OFFSET 10;
+SELECT * FROM profesores LIMIT 5 OFFSET 15;
+SELECT * FROM profesores LIMIT 5 OFFSET 20;
+SELECT * FROM profesores LIMIT 5 OFFSET 25;
+
+
+-- Unico orden para utilizar SELECT con todas sus opciones
+SELECT columna1, columna2, count(*)
+FROM tabla
+WHERE condicion1 AND condicion2 OR condicion3
+GROUP BY columna1, columna2
+ORDER BY columna1 ASC, columna2 DESC
+LIMIt 1
+OFFSET 0;
+
+
+-- Mostrar todos los profesores que esten activos
+
+-- Mostrar todos los profesores que tengan una fecha de contratacion del año pasado
+
+-- Mostrar todos los profesores que se llamen Susan o se apelliden King
+
+-- Mostrar todos los profesores cuyo correo sea hotmail.com o outlook.com o example.com
+
+-- Mostrar el total de profesores que tengan correo gmail.com
